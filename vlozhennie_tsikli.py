95a5faff3cd36f605f3a988fadc1673353420e99
Leonid <118808870+lionsmode@users.noreply.github.com>
2025-12-25 09:39:54 +0900

    Update vlozhennie_tsikli.py

--- vlozhennie_tsikli.py
+++ vlozhennie_tsikli.py
@@ -38,7 +38,7 @@ print(sistema())
 """
 
 #3
-
+"""
 def sistema():
     for x in range(10):
         num1 = x * (17**4) + 11 * (17**3) + 0 * (17**2) + 9 * (17**1) + 7
@@ -52,3 +52,35 @@ def sistema():
 
 if __name__ == "__sistema__":
     sistema()
+"""
+
+#4
+"""def sistema():
+    x = 0
+    y = 0
+    mini = 0
+
+    while y < 8:
+        while x < 8:
+            s = int(f"{y}04{x}5", 11) + int(f"253{x}{y}", 8)
+
+            if s % 117 == 0:
+                     mini = s // 117
+
+            x += 1
+        x = 0
+        y += 1
+    return mini
+print(sistema())"""
+
+#5
+"""def sistema():
+    a = 7 * (512 ** 1912)
+    b = 6 * (64 ** 1954)
+    c = 5 * (8 ** 1991)
+    d = 4 * (8 ** 1980)
+    value = a + b - c - d - 2022
+    value_oct = oct(value)[2:]
+    return value_oct.count('7')
+otvet = sistema()
+print(otvet)"""
